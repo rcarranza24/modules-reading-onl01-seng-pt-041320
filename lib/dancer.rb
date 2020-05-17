cb8535bdee24af2dc063828e137044124c52c79c
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-17 00:49:17 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,13 +2,11 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+  attr_accessor :name
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
   include Dance
   extend MetaDancing
-  
-  
-  attr_accessor :name
   
   def initialize(name)
     @name = name
