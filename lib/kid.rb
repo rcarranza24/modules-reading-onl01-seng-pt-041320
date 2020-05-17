1616c35717968df36bb6680506256a6a1ea60ebe
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-17 00:49:30 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,13 +2,11 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  attr_accessor :name
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
   include Dance
   extend MetaDancing
-  
-  
-  attr_accessor :name
   
   def initialize(name)
     @name = name
