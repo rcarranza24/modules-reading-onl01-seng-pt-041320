3cb74999f5ad1c5b023e29b581ab5e6e7fb76f07
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-17 00:50:48 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  attr_accessor :name
+  include Dance
+  extend MetaDancing
+  
+  def initialize(name)
+    @name = name
+  end
+
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  attr_accessor :name
+  include Dance
+  extend MetaDancing
+  
+  def initialize(name)
+    @name = name
+  end
+end
